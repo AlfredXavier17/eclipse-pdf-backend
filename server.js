5fd297dde13e93c1cbc9103606679f7f7e57d632
Alfred Xavier C.T <Alfredxavierct@gmail.com>
2025-11-23 19:22:32 -0600

    added some env

--- server.js
+++ server.js
@@ -12,11 +12,12 @@ require("dotenv").config();
 // 🔥 Firebase Admin Setup
 // =========================
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
 
 const db = admin.firestore();
 
